6d67d56f7e8ca63f5f07671a67b2933dbe499332
Magnus Therkelsen <magnus@thrklsn.dk>
2019-03-27 11:20:01 +0100

    Update code for this week's assignment

--- opg6/script.m
+++ opg6/script.m
@@ -1,16 +1,35 @@
-clear variables;
+%% Restart
+clear all;
 close all;
 clc;
 
-num = [6.424];
-denom = [1 6.892];
-G = tf(num, denom);
+%% Parameters
+
+% Process transfer function
+num = [6249];
+denum = [1 1904 6718];
+G = tf(num, denum);
+
+% figure;
+% bode(G);
+% grid on;
 
 MAX_MOTOR_VOLTAGE = 3;
-K_p = 0.45;
-K_i =0.002;
+K_p = 9.8855;
+% tau_i = 0.0132;
+tau_i = 50;
+K_i = 1/tau_i;
+alpha = 0.20;
+% tau_d = 0.0105;
+tau_d = 0.3472;
 
-sim('wheelsdown_sim', 20);
+%%
+G_c = tf([6249*tau_i*tau_d, 6249*(tau_i+tau_d), 6249], [alpha*tau_i*tau_d, (1904*alpha*tau_i*tau_d + tau_i), (6718*alpha*tau_i*tau_d + 1904*tau_i), 6718*tau_i])
+
+G*G_c
+%%
+% sim('wheelsdown_sim', 5);
+sim('wheelsdown_sim_pi_lead', 20);
 
 %% Plotting
 figure;
@@ -22,7 +41,7 @@ title('Regulator step response');
 xlabel('Time [s]');
 ylabel('Position [m]');
 grid('on');
-legend('Position step', 'Step response');
+legend('Position step', 'Step response', 'Location', 'best');
 
 %%
 % open_system('wheelsdown_sim');
